0ed65c9228391b2e8e8a397d0ae0168525eb8f84
Paolo Brasolin <paolo.brasolin@gmail.com>
2016-01-31 22:45:40 +0100

    tweaked build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # only proceed script when started not by pull request (PR)
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  echo "this is PR, exiting"
-  exit 0
-fi
+#if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+#  echo "this is PR, exiting"
+#  exit 0
+#fi
 
 # enable error reporting to the console
 set -e
